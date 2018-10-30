b28fe7da8fa9ce2a05172e3593ac86515db35432
Frank Ang <frankang@amazon.com>
2018-10-30 19:01:09 +0800

    time duration based loop

--- simple-canary.py
+++ simple-canary.py
@@ -3,19 +3,29 @@ import os
 import sys
 import time
 import requests
+from datetime import datetime
+from datetime import timedelta
 
-MAX_LOOPS=3
-SLEEP_SECS=1
+SLEEP_SECS=10
+MAX_HOURS=1
+MAX_LOOPS=60*60*MAX_HOURS/SLEEP_SECS
 
 def canary_main(endpoint):
     print ("Canary Main...")
+
+    end_time=datetime.now() + timedelta(hours=MAX_HOURS)
+
     print ("Begin Loop...")
-    for i in range(MAX_LOOPS):
+
+    while true:
+        if datetime.now() > end_time:
+            break
         if i>0: # don't sleep on the first iteration.
             time.sleep(SLEEP_SECS) # no jitter
-        print("do stuff.")
+        print("Calling endpoint: " + endpoint)
         resp = requests.get('https://' + endpoint + '/')
         print (resp)
+
     print ("End Loop...")
 
 if __name__ == '__main__':
